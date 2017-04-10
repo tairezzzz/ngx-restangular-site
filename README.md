344ea188a7134a8a975f283de6d8357d3d1e8de4
smirnoffnew <smirnoffnew@gmail.com>
2017-04-10 13:15:21 +0300

    first worck commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Arest
+# Minimal Starter with Angular on both Server and Browser Platforms
+
+## Get Started
+```sh
+npm run start
+```
+## Developement mode
+* Terminal 1: ```npm run watch```
+* Wait for the build to finish
+* Terminal 2: ```npm run server```
+
+## Prod mode
+Includes AoT
+```sh
+npm run build:prod
+npm run server
+```
+
+Based on https://github.com/robwormald/ng-universal-demo
+
---
+++ src/app/home/home-view.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { TransferHttp } from '../../modules/transfer-http/transfer-http';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+	selector: 'home-view',
+	//template: `<h3>{{subs | async}}</h3>`
+  templateUrl: 'home-view.component.html'
+})
+export class HomeView implements OnInit {
+  public subs: Observable<string>;
+
+  constructor(private http: TransferHttp) {}
+
+  ngOnInit() {
+    this.subs = this.http.get('http://localhost:8000/data').map(data => {
+      return `${data.greeting} ${data.name}`;
+    });
+  }
+}
