e8aca7dfcfc06def62ee8d224aa0a0ca93ea5f6f
Aleksander <t.fortus@gmail.com>
2018-01-18 20:05:23 +0200

    fixed<benefits-section> full adaptive vers.1.0

--- src/app/main/home/benefits-section/benefits-section.component.ts
+++ src/app/main/home/benefits-section/benefits-section.component.ts
@@ -10,50 +10,93 @@ import { Component, OnInit } from '@angular/core';
 export class BenefitsSectionComponent implements OnInit {
   
   BenefitsList: {
+    bgPath: string,
     imgPath: string,
     imgName: string,
+    imgBorderModifier: string,
+    bgSizeModifier: string,
+    picSizeModifier: string,
+    itemBorderModifier: string,
     title: string,
-    text: string
+    titleBorderModifier: string,
+    text: string,
   }[];
   
   ngOnInit() {
     
     this.BenefitsList = [
       {
-        imgPath: '/assets/img/svg/bg-1.svg',
+        bgPath: '/assets/img/svg/bg-1.svg',
+        imgPath: '/assets/img/svg/parallax-1.svg',
         imgName: '',
+        imgBorderModifier: 'benefits-list__img_border_1',
+        bgSizeModifier: 'benefits-list__img-bg_size_1',
+        picSizeModifier: 'benefits-list__img-pic_size_1',
+        itemBorderModifier: 'benefits-list__item_border_1',
         title: 'Send requests easily using different settings',
-        text: 'Configure an application to use different settings in different parts of your application. Generate different requests to different services without troubles.'
-  },
+        titleBorderModifier: 'benefits-list__title_border_1',
+        text: 'Configure an application to use different settings in different parts of your application. Generate different requests to different services without troubles.',
+      },
       {
-        imgPath: '/assets/img/svg/bg-2.svg',
+        bgPath: '/assets/img/svg/bg-2.svg',
+        imgPath: '/assets/img/svg/parallax-2.svg',
         imgName: '',
+        imgBorderModifier: 'benefits-list__img_border_2',
+        bgSizeModifier: 'benefits-list__img-bg_size_2',
+        picSizeModifier: 'benefits-list__img-pic_size_2',
+        itemBorderModifier: 'benefits-list__item_border_2',
         title: 'Send request from within an object',
-        text: 'Once you have got an object as an response for an URL you are able to send further quests using that object. No need to specify the URL each time.'
+        titleBorderModifier: 'benefits-list__title_border_2',
+        text: 'Once you have got an object as an response for an URL you are able to send further quests using that object. No need to specify the URL each time.',
       },
       {
-        imgPath: '/assets/img/svg/bg-3.svg',
+        bgPath: '/assets/img/svg/bg-3.svg',
+        imgPath: '/assets/img/svg/parallax-3.svg',
         imgName: '',
+        imgBorderModifier: 'benefits-list__img_border_3',
+        bgSizeModifier: 'benefits-list__img-bg_size_3',
+        picSizeModifier: 'benefits-list__img-pic_size_3',
+        itemBorderModifier: 'benefits-list__item_border_3',
         title: 'Use meaningful names instead of URLs',
-        text: 'Once you receive an object from an URL you can use meaningful names to access data next time. There is no need to remember each URL you use.'
+        titleBorderModifier: 'benefits-list__title_border_3',
+        text: 'Once you receive an object from an URL you can use meaningful names to access data next time. There is no need to remember each URL you use.',
+
       },
       {
-        imgPath: '/assets/img/svg/bg-4.svg',
+        bgPath: '/assets/img/svg/bg-4.svg',
+        imgPath: '/assets/img/svg/parallax-4.svg',
         imgName: '',
+        imgBorderModifier: 'benefits-list__img_border_4',
+        bgSizeModifier: 'benefits-list__img-bg_size_4',
+        picSizeModifier: 'benefits-list__img-pic_size_4',
+        itemBorderModifier: 'benefits-list__item_border_4',
         title: 'Specify custom HTTP methods',
-        text: 'It is possible to add custom HTTP methods. Add unique logic to a custom method and receive what you need with a server response.'
+        titleBorderModifier: 'benefits-list__title_border_4',
+        text: 'It is possible to add custom HTTP methods. Add unique logic to a custom method and receive what you need with a server response.',
       },
       {
-        imgPath: '/assets/img/svg/bg-5.svg',
+        bgPath: '/assets/img/svg/bg-5.svg',
+        imgPath: '/assets/img/svg/parallax-5.svg',
         imgName: '',
+        imgBorderModifier: 'benefits-list__img_border_5',
+        bgSizeModifier: 'benefits-list__img-bg_size_5',
+        picSizeModifier: 'benefits-list__img-pic_size_5',
+        itemBorderModifier: 'benefits-list__item_border_5',
         title: 'Work with nested RESTful resources',
-        text: 'Ngx-restantugal has built-in features to work with nested entities.'
+        titleBorderModifier: 'benefits-list__title_border_5',
+        text: 'Ngx-restantugal has built-in features to work with nested entities.',
       },
       {
-        imgPath: '/assets/img/svg/bg-6.svg',
+        bgPath: '/assets/img/svg/bg-6.svg',
+        imgPath: '/assets/img/svg/parallax-6.svg',
         imgName: '',
+        imgBorderModifier: 'benefits-list__img_border_6',
+        bgSizeModifier: 'benefits-list__img-bg_size_6',
+        picSizeModifier: 'benefits-list__img-pic_size_6',
+        itemBorderModifier: 'benefits-list__item_border_6',
         title: 'Do not create new object for each request',
-        text: 'Send first request, receive an object and use it further to make server requests instead of creating new objects each time.'
+        titleBorderModifier: 'benefits-list__title_border_6',
+        text: 'Send first request, receive an object and use it further to make server requests instead of creating new objects each time.',
       }
     ];
   }
