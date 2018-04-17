1cbc0f2b766dab32973fc60dfe8c8a03b2e47631
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-04-17 17:55:26 +0300

    fixed<header-socials> added aria-label, role and rel attributes

--- src/app/main/components/header/header.component.ts
+++ src/app/main/components/header/header.component.ts
@@ -11,6 +11,7 @@ export class HeaderComponent implements OnInit {
  headerSocialsList: {
    imgPath: string,
    linkPath: string,
+   ariaLabel: string,
    modifier: boolean
  }[];
   
@@ -22,26 +23,31 @@ export class HeaderComponent implements OnInit {
       {
         imgPath: '/assets/img/svg/sprite.min.svg#fb',
         linkPath: 'https://www.facebook.com/2muchcoffeecom',
+        ariaLabel: '2muchcoffee company facebook page',
         modifier: false
       },
       {
         imgPath: '/assets/img/svg/sprite.min.svg#in',
         linkPath: 'https://www.linkedin.com/company/2muchcoffee',
+        ariaLabel: '2muchcoffee company linkedin page',
         modifier: false
       },
       {
         imgPath: '/assets/img/svg/sprite.min.svg#github',
         linkPath: 'https://github.com/2muchcoffeecom',
+        ariaLabel: '2muchcoffee company github page',
         modifier: true
       },
       {
         imgPath: '/assets/img/svg/sprite.min.svg#behance',
         linkPath: 'https://www.behance.net/2muchcoffee',
+        ariaLabel: '2muchcoffee company behance page',
         modifier: true
       },
       {
         imgPath: '/assets/img/svg/sprite.min.svg#tw',
         linkPath: 'https://twitter.com/2muchcoffeecom',
+        ariaLabel: '2muchcoffee company twitter page',
         modifier: false
       }
     ];
